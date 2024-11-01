f1cff879b4c548c1eff764f376c6ca0537378109
Your Name <your-email@example.com>
2024-11-01 08:35:56 +0500

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/Hepta-frontend/',
+  base : '/Hepta-project/',
   plugins: [react()],
 })
