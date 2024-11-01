c7932779272d66f6999e0b946262b8026bba7d13
Your Name <your-email@example.com>
2024-11-01 13:32:11 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/Hepta-project/',
   plugins: [react()],
+  base : '/Hepta-project/',
 })
